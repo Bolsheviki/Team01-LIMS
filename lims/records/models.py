30bdfd7dda79be7f7b37d1bab91f65781f82e0cc
Da Huang <Bolsheviki>
2012-05-22 20:54:21 +0800

    modified:   records/models.py

--- lims/records/models.py
+++ lims/records/models.py
@@ -2,35 +2,67 @@ from django.db import models
 
 
 # Create your models here.
-class Publisher(models.Model):
-    name = models.CharField(max_length=30)
-    address = models.CharField(max_length=50)
-    city = models.CharField(max_length=60)
-    state_province = models.CharField(max_length=30)
-    country = models.CharField(max_length=50)
-    website = models.URLField()
+class Book(models.Model):
+    isbn = models.BigIntegerField();
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=30)
+    retrieval = models.CharField(max_length=30)
+    publisher = models.CharField(max_length=100)
+    abstract = models.TextField(blank=True, max_length=500)
+    image = models.ImageField(upload_to=None)
+    
+    authors = models.ManyToManyField(Author)
 
     def __unicode__(self):
         return self.name
 
-    class Meta:
-        ordering = ['name']
+
+class BookInstance(models.Model):
+    STATE_CHOICES = (
+        (u'B', u'Borrowed'),
+        (u'U', u'UnBorrowed'),
+        (u'D', u'Discard'),
+    )
+    book = models.ForeignKey(Book)
+    state = models.CharField(max_length=10, choices=STATE_CHOICES)
+    renewal = models.BooleanField()
+
+
+class User(models.Model):
+    UTYPE_CHOICES = (
+        (u'N', u'Normal User'),
+        (u'C', u'Cpunter Administrator'),
+        (u'U', u'User Administrator'),
+        (u'B', u'Book Administrator'),
+    )
+    LEVEL_CHOICES = (
+        (u'U', u'undergraduate'),
+        (u'G', u'graduate'),
+        (u'S', u'staff'),
+    )
+    uid = models.CharField(max_length=20)
+    name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=50)
+    password = models.CharField(max_length=20)
+    utype = models.CharField(choices=UTYPE_CHOICES)
+    level = models.CharField(choices=LEVEL_CHOICES)
+    debt = models.IntegerField()
+
+
+class Recored(models.Model):
+    book = models.ForeignKey(Book)
+    user = models.ForeignKey(User)
+    time = models.DateTimeField(auto_now=True)
 
 
 class Author(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=40)
-    email = models.EmailField(blank=True, verbose_name='e-mail')
+    ROLE_CHOICES = (
+        (u'W', u'Writer'),
+        (u'T', u'Translator'),
+    )
+    name = models.CharField(max_length=30)
+    role = models.CharField(max_length=2, choices=ROLE_CHOICES)
     
     def __unicode__(self):
-        return u'%s %s' % (self.first_name, self.last_name)
+        return u'%s %s' % (self.name, self.role)
 
-
-class Book(models.Model):
-    title = models.CharField(max_length=100)
-    authors = models.ManyToManyField(Author)
-    publisher = models.ForeignKey(Publisher)
-    publication_date = models.DateField()
-
-    def __unicode__(self):
-        return self.title
