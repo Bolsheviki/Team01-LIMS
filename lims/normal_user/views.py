f982a447b48209e4fd538537dd2303faab8e90fb
Da Huang <Bolsheviki>
2012-06-05 20:49:30 +0800

    fix

--- lims/normal_user/views.py
+++ lims/normal_user/views.py
@@ -75,7 +75,6 @@ def information(request):
     except:
         relist = paginator.page(1)
         
-        
     return render_to_response('normal_user/information.html',
         {'app':app,
         'username':username,
@@ -89,6 +88,7 @@ def information(request):
         context_instance=RequestContext(request)
     )
 
+    
 @user_passes_test(is_normal_user_logged_in, login_url = '/normal-user/login/')
 def borrowbook(request):
     app = 'normal-user'
