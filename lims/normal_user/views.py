0bb2591bb9cbb7e2fd1328df52868c67421ca911
Da Huang <Bolsheviki>
2012-06-05 20:29:05 +0800

    adjust

--- lims/normal_user/views.py
+++ lims/normal_user/views.py
@@ -26,7 +26,7 @@ def search(request):
 
 @user_passes_test(is_normal_user_logged_in, login_url = '/normal-user/login/')
 def settings(request):
-    return settings_in_template(request, 'normal_user/settings.html')
+    return settings_in_template(request, 'normal_user/settings.html', 'normal-user')
     
 
 @user_passes_test(is_normal_user_logged_in, login_url = '/normal-user/login/')
