356270c0255853599932df5c53a5f16799b97cd4
Da Huang <Bolsheviki>
2012-06-13 12:34:06 +0800

    fix book admin

--- lims/book_admin/forms.py
+++ lims/book_admin/forms.py
@@ -9,17 +9,21 @@ def validate_book_isbn(isbn):
     book = util.info_book(isbn)
     if not book:
         raise forms.ValidationError(u'ISBN error')
-
-
+        
 class AddBookForm(forms.Form):
     isbn = forms.CharField(validators=[validate_book_isbn])
 
     
 def validate_book_id(id):
     try:
-        book = BookInstance.objects.get(Q(id=id)&Q(removed=False))
+        book = BookInstance.objects.get(id=id)
     except:
-        raise forms.ValidationError(u'Book ID error')
+        raise forms.ValidationError(u'Book ID error.')
+    if book.state == 'R':
+        raise forms.ValidationError(u'This book has been removed.')
+    elif book.state == 'B':
+        raise forms.ValidationError(u'This book has been borrowed.')
     
 class RemoveBookForm(forms.Form):
     bookId = forms.CharField(validators=[validate_book_id])
+    
